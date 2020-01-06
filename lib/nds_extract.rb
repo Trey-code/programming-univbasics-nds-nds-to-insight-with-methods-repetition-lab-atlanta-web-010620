bde0c944276c9e6ae595f290b009e129bb79a18d
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 13:34:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,9 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+ stooges = [{:name => "Larry"}, {:name => "Curly"}, {:name => "Moe"}, {:name => "Iggy"}]
+    expect(list_of_directors(stooges)).to eq(["Larry", "Curly", "Moe", "Iggy"])
+  end
 end
 
 def total_gross(source)
